d98786a8028f1792a3656ae3b623c6aba6ee7e25
Pedro <pmedina.c95@gmail.com>
2016-12-15 01:06:56 -0700

    Version final, ya para entregar

--- IncidenciasApp/app/src/main/java/com/example/usuario/incidenciasapp/adapters/UsuarioAdapter.java
+++ IncidenciasApp/app/src/main/java/com/example/usuario/incidenciasapp/adapters/UsuarioAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.usuario.incidenciasapp.models.Equipo;
@@ -21,10 +23,12 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.UsuarioV
 
     private ArrayList<Usuario> items;
     private Context context;
+    private boolean isUsuario;
 
-    public UsuarioAdapter(Context context, ArrayList<Usuario> items) {
+    public UsuarioAdapter(Context context, ArrayList<Usuario> items, boolean isUsuario) {
         this.items = items;
         this.context = context;
+        this.isUsuario = isUsuario;
     }
 
     @Override
@@ -37,6 +41,11 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.UsuarioV
     public void onBindViewHolder(UsuarioAdapter.UsuarioViewHolder holder, int i) {
         final Usuario usuario = items.get(i);
         holder.tvCorreo.setText(usuario.getCorreo());
+        if(isUsuario) {
+            holder.tvEspecialidad.setVisibility(View.GONE);
+            holder.tvEsfuerzo.setVisibility(View.GONE);
+            holder.imageView3.setVisibility(View.GONE);
+        }
         holder.tvEspecialidad.setText(usuario.getEspecialidad());
         holder.tvNombreUsuario.setText(usuario.getNombre());
         try {
@@ -46,6 +55,7 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.UsuarioV
             holder.tvEquipo.setText("Usuario sin equipo asignado");
 
         }
+
         holder.tvEsfuerzo.setText(String.valueOf(usuario.getEsfuerzo()));
         switch (usuario.getTipoUsuario()) {
             case Usuario.TIPO_ADMINISTRADOR:
@@ -77,6 +87,7 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.UsuarioV
         TextView tvCorreo;
         TextView tvEspecialidad;
         TextView tvEsfuerzo;
+        ImageView imageView3;
 
 
         public UsuarioViewHolder(View view){
@@ -87,6 +98,7 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.UsuarioV
             tvCorreo = (TextView) view.findViewById(R.id.tv_email_usuario);
             tvEspecialidad = (TextView) view.findViewById((R.id.tv_especialidad));
             tvEsfuerzo = (TextView) view.findViewById(R.id.tv_esfuerzo);
+            imageView3 = (ImageView) view.findViewById(R.id.imageView3);
         }
     }
 }
